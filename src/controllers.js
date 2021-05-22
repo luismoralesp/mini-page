9f74c0663c0aedd1002ecb45249e302315f22865
Luis Morales <lmorales@condorlabs.io>
2021-05-22 18:06:23 -0500

    test

--- src/controllers.js
+++ src/controllers.js
@@ -4,7 +4,7 @@ const { parse } = require('node-html-parser');
 const { PORT = 8000 } = process.env;
 
 const parseTemplate = async (fullhost, config_name, path) => {
-  //const config = await axios.get(`${fullhost}/configs/${config_name}.json`);
+  const config = await axios.get(`${fullhost}/configs/${config_name}.json`);
 
   /*if (!path) {
     path = config.template;
@@ -26,7 +26,7 @@ const parseTemplate = async (fullhost, config_name, path) => {
 
   return root.toString();*/
 
-  return `${fullhost}/configs/${config_name}.json`;
+  return config.data;
 };
 
 const pathController = (req, res) => {
