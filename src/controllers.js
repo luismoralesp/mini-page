9fbca79cf8b45d77aec316c760c05f2136aa6c33
Luis Morales <lmorales@condorlabs.io>
2021-05-22 17:50:48 -0500

    test

--- src/controllers.js
+++ src/controllers.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
-
+const axios = require('axios');
 const { parse } = require('node-html-parser');
 
-const parseTemplate = (config_name, path) => {
-  const config = require(`${__dirname}/configs/${config_name}.json`);
+const { PORT=8000 } = process.env
+
+const parseTemplate = async (fullhost, config_name, path) => {
+  const config = await axios.get(`${fullhost}/configs/${config_name}.json`);
 
   /*if (!path) {
     path = config.template;
@@ -25,7 +26,7 @@ const parseTemplate = (config_name, path) => {
 
   return root.toString();*/
 
-  return "<h1>HELLO!</h1>"
+  return config.data
 };
 
 const pathController = (req, res) => {
@@ -37,8 +38,8 @@ const pathController = (req, res) => {
 
 const indexController = (req, res) => {
   const hostname = req.hostname;
-
-  res.send(parseTemplate(hostname));
+  const fullhost = `${req.protocol}://${req.host}:${PORT}`
+  parseTemplate(fullhost,hostname).then(r => res.send(r)) ;
 };
 const health = (req, res) => {
   res.send('Ok.');
--- src/routers.js
+++ src/routers.js
@@ -7,6 +7,7 @@ const mainRouter = new Router();
 mainRouter.get('/health', health);
 mainRouter.use('/static', express.static(__dirname + '/static'));
 mainRouter.use('/templates', express.static(__dirname + '/templates'));
+mainRouter.use('/configs', express.static(__dirname + '/configs'));
 mainRouter.get('/:path', pathController);
 mainRouter.get('/', indexController);
 
